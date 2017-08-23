5dc8fd33ea633af20f9aff4cc2d79ae9a6daf9c5
Adrian Smuzniak <adriansmuzniak@gmail.com>
2017-08-23 12:41:45 +0200

    fire method creation

--- js/battleship.js
+++ js/battleship.js
@@ -22,15 +22,20 @@ var view = {
 var model = {
   boardSize: 7,
   numShips: 3,
-  shipLength: 3;
+  shipLength: 3,
   shipSunk: 0,
   ships: [{ locations: ["10", "20", "30"], hits: ["", "", ""] },
-    { locations: ["32", "33", "34"], hits: ["", "hit", ""] },
-    { locations: ["63", "64", "65"], hits: ["hit", "", ""] }],
+    { locations: ["32", "33", "34"], hits: ["", "", ""] },
+    { locations: ["63", "64", "65"], hits: ["", "", ""] }],
   fire: function(guess) {
     for (var i = 0; i <this.numShips; i++) {
       var ship = this.ships[i];
+      locations = ship.locations;
+      var index = locations.indexOf(guess); //not "-1" if found
+      if (index >= 0) { //hit!
+        ships.hits[index] = "hit";
+        return true;
+      }
     }
   }
-
 }
