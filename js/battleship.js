8b6eb3c8623a1b6e57b1bfa3491bf41f8871db52
Adrian Smuzniak <adriansmuzniak@gmail.com>
2017-08-23 13:05:25 +0200

    methods tests creation

--- js/battleship.js
+++ js/battleship.js
@@ -28,14 +28,37 @@ var model = {
     { locations: ["32", "33", "34"], hits: ["", "", ""] },
     { locations: ["63", "64", "65"], hits: ["", "", ""] }],
   fire: function(guess) {
-    for (var i = 0; i <this.numShips; i++) {
+    for (var i = 0; i < this.numShips; i++) {
       var ship = this.ships[i];
       // locations = ship.locations;
       var index = ship.locations.indexOf(guess); //not "-1" if found
       if (index >= 0) { //hit!
         ships.hits[index] = "hit";
+        view.displayHit(guess);
+        view.displayMessage("Trafiony!")
+        if (this.isSunk(ship)) {
+          wiev.displayMessage("Zatopiłeś mój okręt!!")
+          shipSunk++;
+        }
         return true;
       }
     }
+    view.displayMiss(guess);
+    view.displayMessage("Pudło!");
+    return false;
+  },
+  isSunk: function(ship) {
+    for (var i = 0; i < this.shipLength; i++) {
+      if (ship.hits[i] !== "hit") {
+        return false;
+      }
+      return true;
+    }
   }
 }
+
+model.fire("21");
+model.fire("51");
+model.fire("45");
+model.fire("52");
+model.fire("24");
